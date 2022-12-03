ed88217ba995005468d8f5ae2d8a9c07d81b0a3d
higa ryuku <tiiyou105@gmail.com>
2022-12-03 16:46:42 +0900

    ポケモンデータ取得処理を実装

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
+import { getAllPokemon } from "./utils/pokemon";
+
 function App() {
-  return <div className="App"></div>;
+  const initial = "https://pokeapi.co/api/v2/pokemon";
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      // 全てのポケモンを取得
+      let res = await getAllPokemon(initial);
+      console.log(res);
+      setLoading(false);
+    };
+    fetchPokemonData();
+  }, []);
+  return (
+    <div className="App">
+      {loading ? (
+        <>
+          <h1>ロード中</h1>
+        </>
+      ) : (
+        <>
+          <h1>ポケモンを取得しました</h1>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
