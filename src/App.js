c49c96949f218777a8902425a1c1ff4ec04e5731
higa ryuku <tiiyou105@gmail.com>
2022-12-03 17:47:16 +0900

    ポケモン詳細情報取得処理を実装

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-import { getAllPokemon } from "./utils/pokemon";
+import { getAllPokemon, getPokemon } from "./utils/pokemon";
 
 function App() {
   const initial = "https://pokeapi.co/api/v2/pokemon";
   const [loading, setLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState([]);
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       // 全てのポケモンを取得
       let res = await getAllPokemon(initial);
-      console.log(res);
+
+      // 各ポケモンの詳細なデータを取得
+      loadPokemon(res.results);
+
       setLoading(false);
     };
     fetchPokemonData();
   }, []);
+
+  const loadPokemon = async (data) => {
+    let _pokemonData = await Promise.all(
+      data.map((pokemon) => {
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+    setPokemonData(_pokemonData);
+  };
+
   return (
     <div className="App">
       {loading ? (
--- src/utils/pokemon.js
+++ src/utils/pokemon.js
@@ -1,4 +1,24 @@
+/**
+ * ポケモンデータの全権取得
+ * @param {*} url
+ * @returns
+ */
 export const getAllPokemon = (url) => {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        resolve(data);
+      });
+  });
+};
+
+/**
+ * ポケモンデータ詳細情報取得
+ * @param {*} url
+ * @returns
+ */
+export const getPokemon = (url) => {
   return new Promise((resolve, reject) => {
     fetch(url)
       .then((res) => res.json())
